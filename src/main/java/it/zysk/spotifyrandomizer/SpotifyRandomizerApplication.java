231dd675c119ae61e7abbb2477dc31e2c3aac6d9
zysktomasz <zysk.tomasz95@gmail.com>
2021-03-29 21:14:31 +0200

    Initial commit

---
+++ src/main/java/it/zysk/spotifyrandomizer/SpotifyRandomizerApplication.java
@@ -0,0 +1,13 @@
+package it.zysk.spotifyrandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRandomizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyRandomizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/zysk/spotifyrandomizer/SpotifyRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.zysk.spotifyrandomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyRandomizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
