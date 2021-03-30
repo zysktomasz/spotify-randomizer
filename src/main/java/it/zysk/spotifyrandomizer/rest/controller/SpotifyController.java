818b36b03ee4b8c05af4bb8d87afa4bcb8934099
zysktomasz <zysk.tomasz95@gmail.com>
2021-03-30 20:17:25 +0200

    Add basic example usage of: getting list of user's playlist & getting tracks from playlist

--- src/main/java/it/zysk/spotifyrandomizer/rest/controller/SpotifyController.java
+++ src/main/java/it/zysk/spotifyrandomizer/rest/controller/SpotifyController.java
@@ -4,9 +4,14 @@ import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
+import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
 import com.wrapper.spotify.model_objects.specification.User;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import com.wrapper.spotify.requests.data.playlists.GetListOfCurrentUsersPlaylistsRequest;
+import com.wrapper.spotify.requests.data.playlists.GetPlaylistsItemsRequest;
 import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 import it.zysk.spotifyrandomizer.rest.config.SpotifyProperties;
 import org.apache.hc.core5.http.ParseException;
@@ -17,12 +22,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 //@RestController
 @Controller
 public class SpotifyController {
 
     private final SpotifyApi spotifyApi;
+    private String currentUserDisplayName;
 
     public SpotifyController(SpotifyProperties spotifyProperties) {
         spotifyApi = SpotifyApi.builder()
@@ -38,7 +49,6 @@ public class SpotifyController {
         AuthorizationCodeUriRequest authorizationCodeRequest = spotifyApi.authorizationCodeUri()
                 .scope("user-read-private user-read-email")
                 .build();
-
 
         URI execute = authorizationCodeRequest.execute();
 
@@ -69,10 +79,53 @@ public class SpotifyController {
         GetCurrentUsersProfileRequest getCurrentUsersProfileRequest = spotifyApi.getCurrentUsersProfile()
                 .build();
 
-        User execute = getCurrentUsersProfileRequest.execute();
+        User currentUser = getCurrentUsersProfileRequest.execute();
 
-        System.out.println(execute);
+        currentUserDisplayName = currentUser.getDisplayName();
 
-        return execute;
+        System.out.println(currentUser);
+
+        return currentUser;
+    }
+
+    @GetMapping("playlists")
+    @ResponseBody
+    public List<PlaylistSimplified> getUserPlaylists() throws ParseException, SpotifyWebApiException, IOException {
+        GetListOfCurrentUsersPlaylistsRequest playlistsRequest = spotifyApi.getListOfCurrentUsersPlaylists()
+                .build();
+
+        Paging<PlaylistSimplified> execute = playlistsRequest.execute();
+
+        // by default returns only public playlists
+        // requires additional scope to access private playlists
+        return Optional.ofNullable(execute.getItems())
+                .stream()
+                .flatMap(Stream::of)
+                .filter(playlistSimplified -> playlistSimplified
+                        .getOwner()
+                        .getDisplayName()
+                        .equals(currentUserDisplayName)
+                )
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("tracks")
+    @ResponseBody
+    public List<PlaylistTrack> getPlaylistTracks(
+            @RequestParam("playlistId") String playlistId
+    ) throws ParseException, SpotifyWebApiException, IOException {
+
+        // either I'm using it wrongly or 'fields' in this request do not work as good
+        // as described here: https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-playlists-tracks
+        GetPlaylistsItemsRequest getPlaylistsItemsRequest = spotifyApi.getPlaylistsItems(playlistId)
+//                .fields("items(added_at,track(album(images),artists(external_urls,name),id,name,track_number))")
+                .build();
+
+        Paging<PlaylistTrack> tracks = getPlaylistsItemsRequest.execute();
+
+        return Optional.ofNullable(tracks.getItems())
+                .stream()
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
     }
 }
