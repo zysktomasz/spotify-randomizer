2fda588b37791c7227838d7f65975be514f1d31e
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-08-06 20:11:28 +0200

    Exclude UserDetailsServiceAutoConfiguration from @SpringBootApplication

--- src/main/java/it/zysk/spotifyrandomizer/SpotifyRandomizerApplication.java
+++ src/main/java/it/zysk/spotifyrandomizer/SpotifyRandomizerApplication.java
@@ -2,8 +2,9 @@ package it.zysk.spotifyrandomizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class SpotifyRandomizerApplication {
 
     public static void main(String[] args) {
