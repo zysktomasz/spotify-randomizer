3ec789476f8cab1399635cc277e45d6be8b39ae4
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-08-15 11:11:58 +0200

    Redirect user to FE after successful authentication

---
+++ src/main/java/it/zysk/spotifyrandomizer/rest/configuration/WebAppProperties.java
@@ -0,0 +1,17 @@
+package it.zysk.spotifyrandomizer.rest.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "webapp")
+@EnableConfigurationProperties
+@Configuration
+@Getter
+@Setter
+public class WebAppProperties {
+    String successfulLoginHandlerUrl;
+}
+
--- src/main/java/it/zysk/spotifyrandomizer/rest/controller/AuthenticationController.java
+++ src/main/java/it/zysk/spotifyrandomizer/rest/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package it.zysk.spotifyrandomizer.rest.controller;
 
+import it.zysk.spotifyrandomizer.rest.configuration.WebAppProperties;
 import it.zysk.spotifyrandomizer.service.authentication.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
@@ -20,12 +21,15 @@ import static it.zysk.spotifyrandomizer.rest.util.ApiConstants.AUTHENTICATION_CO
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+    private final WebAppProperties webAppProperties;
 
     private static final String LOGIN_ENDPOINT = "login";
     private static final String CALLBACK_ENDPOINT = "callback";
 
     private static final String CODE_REQUEST_PARAM = "code";
     private static final String ERROR_REQUEST_PARAM = "error";
+
+    private static final String QUERY_PARAM_JWT = "jwt";
 
     @GetMapping(LOGIN_ENDPOINT)
     public String login() {
@@ -35,7 +39,6 @@ public class AuthenticationController {
     }
 
     @GetMapping(CALLBACK_ENDPOINT)
-    @ResponseBody
     public String callback(
             @RequestParam(value = CODE_REQUEST_PARAM, required = false) String code,
             @RequestParam(value = ERROR_REQUEST_PARAM, required = false) String error) {
@@ -44,6 +47,11 @@ public class AuthenticationController {
             // TODO: 06.07.2021 handle authentication error (user might've denied access or another reason)
         }
 
-        return this.authenticationService.authenticateUser(code);
+        var userJwt = this.authenticationService.authenticateUser(code);
+        var uriComponentsBuilder = UriComponentsBuilder
+                .fromHttpUrl(webAppProperties.getSuccessfulLoginHandlerUrl());
+        uriComponentsBuilder.queryParam(QUERY_PARAM_JWT, userJwt);
+
+        return "redirect:" + uriComponentsBuilder.toUriString();
     }
 }
